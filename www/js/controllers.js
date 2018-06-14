c18660faaf13962bf92e56b08527a13fe63dc3f9
ala <gh.alaeddine@gmail.com>
2018-06-14 20:02:49 +0200

    avant map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -149,4 +149,25 @@ $http({
   this.question = response.data;
   $scope.responses = response.data;
 });
-});
+})
+.controller("MarkerController", [ '$scope', function($scope) {
+  angular.extend($scope, {
+      osloCenter: {
+          lat: 59.91,
+          lng: 10.75,
+          zoom: 12
+      },
+      markers: {
+          osloMarker: {
+              lat: 59.91,
+              lng: 10.75,
+              message: "I want to travel here!",
+              focus: true,
+              draggable: false
+          }
+      },
+      defaults: {
+          scrollWheelZoom: false
+      }
+  });
+}]);
